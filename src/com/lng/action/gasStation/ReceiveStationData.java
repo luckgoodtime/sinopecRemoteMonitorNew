59857e6014eae57308cc9fb99178ea3c8f71bcf9
dfsoft <dfsoft@DESKTOP-RBT16UJ>
2019-05-24 11:33:36 +0800

    增加一个测试方法，测试了json解析

--- src/com/lng/action/gasStation/ReceiveStationData.java
+++ src/com/lng/action/gasStation/ReceiveStationData.java
@@ -101,6 +101,44 @@ public class ReceiveStationData extends BaseController{
 	}
 	
 	
+	public static <T> void main(String[] args) {
+		
+		String s = "{ recordType: \"FillingRecord\", stationNo: \"L02-01-0001\", stationName: \"朗润测试站\", data: [{ \"gunNo\": \"01\", \"cardNo\": \"01000411200000000003\", \"holderName\": \"张三\", \"plateNo\": \"津A12345\", \"fillTime\": \"2019-05-11 20:55:04\", \"Price\": \"5.23\", \"volume\": \"34.67\", \"receivable\": \"181.32\", \"discount\": \"0.32\", \"receiptTotal\": \"181.00\", \"cardBalance\": \"4565.65\", \"cardType\": \"员工卡\", \"fillType\": \"LNG\", \"ttc\": \"2019051100001\", \"note\": \" \"},{ \"gunNo\": \"04\", \"cardNo\": \"01000111200000000002\", \"holderName\": \"李四\", \"plateNo\": \"津A67890\", \"fillTime\": \"2019-05-12 10:33:56\", \"Price\": \"4.25\", \"volume\": \"14.06\", \"receivable\": \"59.76\", \"discount\": \"0.76\", \"receiptTotal\": \"59.00\", \"cardBalance\": \"1233.02\", \"cardType\": \"用户卡\", \"fillType\": \"CNG\", \"ttc\": \"2019051200021\", \"note\": \" \"}] }";
+		
+		JSONObject obj = JSON.parseObject(s);
+		
+		String recordType = obj.getString("recordType");
+		String stationNo = obj.getString("stationNo");
+		String stationName = obj.getString("stationName");
+		
+		JSONArray data = (JSONArray)obj.get("data");
+		
+		List<T> list = null;
+		list = (List<T>) ReceiveStationData.jsonToList(data.toJSONString(), FillingRecord.class);
+		
+		if(list != null && list.size() > 0) {
+			for(Object bean : list) {
+				try {
+					BeanUtils.copyProperty(bean, "stationNo", stationNo);
+					BeanUtils.copyProperty(bean, "stationName", stationName);
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			System.out.println("记录数:" + list.size());
+			System.out.println(((FillingRecord)list.get(0)).getStationName());
+			System.out.println(((FillingRecord)list.get(0)).getStationNo());
+			System.out.println(((FillingRecord)list.get(0)).getPrice());
+			System.out.println(((FillingRecord)list.get(0)).getVolume());
+			System.out.println(((FillingRecord)list.get(0)).getReceivable());
+			System.out.println(((FillingRecord)list.get(0)).getTtc());
+			System.out.println(((FillingRecord)list.get(0)).getCardType());
+		}
+	}
+	
 	
 
 }
