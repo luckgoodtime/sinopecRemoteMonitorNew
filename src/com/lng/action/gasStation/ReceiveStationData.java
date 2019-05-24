a3f1dc806e6e49f9d2ff0f1613e0808ed2b5ee52
dfsoft <dfsoft@DESKTOP-RBT16UJ>
2019-05-24 11:18:14 +0800

    完成数据解析与保存

--- src/com/lng/action/gasStation/ReceiveStationData.java
+++ src/com/lng/action/gasStation/ReceiveStationData.java
@@ -1,11 +1,13 @@
 package com.lng.action.gasStation;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +18,9 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.lng.action.main.BaseController;
 import com.lng.model.remotemonitor.FillingRecord;
+import com.lng.model.remotemonitor.PriceList;
+import com.lng.model.remotemonitor.RechargeRecord;
+import com.lng.model.remotemonitor.ShiftRecord;
 
 @Controller
 @RequestMapping("/gasStation")
@@ -24,6 +29,7 @@ public class ReceiveStationData extends BaseController{
 
 	
 	/**
+	 * @param <T>
 	 * @category 接收站级数据<br/>
 	 * 
 	 * 
@@ -36,13 +42,11 @@ public class ReceiveStationData extends BaseController{
 
 	 * 
 	 * 
-	 * 
-	 * 
-	 * 
 	 */
+	@SuppressWarnings("unchecked")
 	@RequestMapping(value="/acceptStationData.do", method = RequestMethod.POST)
 	@ResponseBody
-	public Map acceptStationData(HttpServletRequest request) {
+	public <T> Map acceptStationData(HttpServletRequest request) {
 	
 		JSONObject obj = JSON.parseObject((String)request.getParameter("p"));
 		
@@ -58,41 +62,45 @@ public class ReceiveStationData extends BaseController{
 		String stationNo = obj.getString("stationNo");
 		String stationName = obj.getString("stationName");
 		
-		JSONArray data = (JSONArray)obj.get("data");	
+		JSONArray data = (JSONArray)obj.get("data");
 		
-		if("FillingRecord".equals(recordType))
-			;
-		else if("PriceList".equals(recordType)) 
-			;
-		else if("RechargeRecord".equals(recordType))
-			;
-		else if("ShiftRecord".equals(recordType))
-			;
+		List<T> list = null;
 		
+		if("FillingRecord".equals(recordType)) {
+			list = (List<T>) ReceiveStationData.jsonToList(data.toJSONString(), FillingRecord.class);
+		} else if("PriceList".equals(recordType)) {			
+			list = (List<T>) ReceiveStationData.jsonToList(data.toJSONString(), PriceList.class);	
+		} else if("RechargeRecord".equals(recordType)) {			
+			list = (List<T>) ReceiveStationData.jsonToList(data.toJSONString(), RechargeRecord.class);	
+		} else if("ShiftRecord".equals(recordType)){
+			list = (List<T>) ReceiveStationData.jsonToList(data.toJSONString(), ShiftRecord.class);
+		}
 		
+		if(list != null && list.size() > 0) {
+			for(Object bean : list) {
+				try {
+					BeanUtils.copyProperty(bean, "stationNo", stationNo);
+					BeanUtils.copyProperty(bean, "stationName", stationName);
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
+			this.baseService.batchSaveOrUpdate(list);
+		}
 		
 		message.put("result", result);
 		return message;
 	}
 	
-	public static void main(String[] args) {
-		String jsonStr = "" + 
-		"{"
-		+ "\"recordType\":\"FillingRecord\","
-		+ "\"stationNo\":\"0101010\","
-		+ "\"stationName\":\"山西晋城XXXX加气站\","
-		+ "\"data\":[{ \"gunNo\" :\"02\",\"cardNo\":\"99991232340001\"}]"
-		+ "}";
-		
-		JSONObject obj = JSON.parseObject(jsonStr);
-		
-		System.out.println(obj.toJSONString());
-		
-		
-		// JSONObect to Java Object
-		
-		//JSONArray.toJavaObject(json, clazz)
-		
-		//System.out.println(record.getGunNo());
+	
+	public static <T> List<T> jsonToList(String jsonStr, Class<T> clazz) {
+		List<T> list = (List<T>) JSON.parseArray(jsonStr, clazz);
+		return list;
 	}
+	
+	
+	
+
 }
