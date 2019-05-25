2fc3d24bfb5cedf59c5c818c8e23cf6bb3432891
floraman <cjhk007@163.com>
2019-05-25 21:58:33 +0800

    4种测试记录

--- src/com/lng/util/OkHttpClientUtil.java
+++ src/com/lng/util/OkHttpClientUtil.java
@@ -54,11 +54,32 @@ public class OkHttpClientUtil {
 		  MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 		  
 		  String s = "{"
-  + "recordType: \"FillingRecord\","
-  + "stationNo: \"L02-01-0001\","
-  + "stationName: \"朗润测试站\","
-  + "data: [{  \"recordId\": \"100\",  \"gunNo\": \"01\",  \"cardNo\": \"01000411200000000003\",  \"holderName\": \"张三\",  \"plateNo\": \"津A12345\",  \"fillTime\": \"2019-05-11 20:55:04\",  \"price\": \"5.23\",  \"volume\": \"34.67\",  \"receivable\": \"181.32\",  \"discount\": \"0.32\",  \"receiptTotal\": \"181.00\",  \"cardBalance\": \"4565.65\",  \"cardType\": \"员工卡\",  \"fillType\": \"LNG\",  \"ttc\": \"2019051100001\",  \"note\": \" \",  \"stationNo\": \"10-0001\",  \"stationName\": \"朗润集团\"},{  \"recordId\": \"101\",  \"gunNo\": \"04\",  \"cardNo\": \"01000111200000000002\",  \"holderName\": \"李四\",  \"plateNo\": \"津A67890\",  \"fillTime\": \"2019-05-12 10:33:56\",  \"price\": \"4.25\",  \"volume\": \"14.06\",  \"receivable\": \"59.76\",  \"discount\": \"0.76\",  \"receiptTotal\": \"59.00\",  \"cardBalance\": \"1233.02\",  \"cardType\": \"用户卡\",  \"fillType\": \"CNG\",  \"ttc\": \"2019051200021\",  \"note\": \" \",  \"stationNo\": \"10-0001\",  \"stationName\": \"朗润集团\"}]"
- +   "}";
+		  + "recordType: \"FillingRecord\","
+		  + "stationNo: \"L02-01-0001\","
+		  + "stationName: \"朗润测试站\","
+		  + "data: [{  \"recordId\": \"100\",  \"gunNo\": \"01\",  \"cardNo\": \"01000411200000000003\",  \"holderName\": \"张三\",  \"plateNo\": \"津A12345\",  \"fillTime\": \"2019-05-11 20:55:04\",  \"price\": \"5.23\",  \"volume\": \"34.67\",  \"receivable\": \"181.32\",  \"discount\": \"0.32\",  \"receiptTotal\": \"181.00\",  \"cardBalance\": \"4565.65\",  \"cardType\": \"员工卡\",  \"fillType\": \"LNG\",  \"ttc\": \"2019051100001\",  \"note\": \" \",  \"stationNo\": \"10-0001\",  \"stationName\": \"朗润集团\"},{  \"recordId\": \"101\",  \"gunNo\": \"04\",  \"cardNo\": \"01000111200000000002\",  \"holderName\": \"李四\",  \"plateNo\": \"津A67890\",  \"fillTime\": \"2019-05-12 10:33:56\",  \"price\": \"4.25\",  \"volume\": \"14.06\",  \"receivable\": \"59.76\",  \"discount\": \"0.76\",  \"receiptTotal\": \"59.00\",  \"cardBalance\": \"1233.02\",  \"cardType\": \"用户卡\",  \"fillType\": \"CNG\",  \"ttc\": \"2019051200021\",  \"note\": \" \",  \"stationNo\": \"10-0001\",  \"stationName\": \"朗润集团\"}]"
+		 +   "}";
+		  
+		  s = "{"
+		  + "recordType: \"ShiftRecord\","
+		  + "stationNo: \"L02-01-0001\","
+		  + "stationName: \"朗润测试站\","
+		  + "data: [{  \"recordId\": \"45\",  \"shiftNo\": \"2019050101\",  \"startTime\": \"2019-05-01 08:00:00\",  \"endTime\": \"2019-05-01 20:00:00\",  \"confirmTime\": \"2019-05-01 20:10:00\",  \"stationNo\": \"10-0001\",  \"stationName\": \"朗润集团\"},{  \"recordId\": \"46\",  \"shiftNo\": \"2019050102\",  \"startTime\": \"2019-05-01 20:00:00\",  \"endTime\": \"2019-05-02 07:59:00\",  \"confirmTime\": \"2019-05-02 08:11:00\",  \"stationNo\": \"10-0001\",  \"stationName\": \"朗润集团\"}]"
+		  + "}";
+		  
+		  s = "{"
+		  + "recordType: \"PriceList\","
+		  + "stationNo: \"L02-01-0001\","
+		  + "stationName: \"朗润测试站\","
+		  + "data: [{  \"recordId\": \"13\",  \"gunNo\": \"02\",  \"oldPrice\": \"4.21\",  \"newPrice\": \"4.23\",  \"publishTime\": \"2019-05-03 11:33:20\",  \"stationNo\": \"10-0001\",  \"stationName\": \"朗润集团\"}]"
+		  + "}";
+		  
+		  s = "{"
+		  + "recordType: \"RechargeRecord\","
+		  + "stationNo: \"L02-01-0001\","
+		  + "stationName: \"朗润测试站\","
+		  + "data: [{  \"recordId\": \"36\",  \"carNo\": \"01000411200000000002\",  \"holderName\": \"李四\",  \"operateTime\": \"2019-05-08 21:22:20\",  \"bizAmount\": \"3600.00\",  \"rebate\": \"0.00\",  \"balance\": \"7200.00\",  \"cardType\": \"用户卡\",  \"bizType\": \"充值\",  \"stationNo\": \"10-0001\",  \"stationName\": \"朗润集团\"},{  \"recordId\": \"37\",  \"carNo\": \"01000411200000000005\",  \"holderName\": \"王五\",  \"operateTime\": \"2019-05-08 23:03:11\",  \"bizAmount\": \"-1200.00\",  \"rebate\": \"0.00\",  \"balance\": \"500.00\",  \"cardType\": \"用户卡\",  \"bizType\": \"扣款\",  \"stationNo\": \"10-0001\",  \"stationName\": \"朗润集团\"},{  \"recordId\": \"38\",  \"carNo\": \"01000511200000000009\",  \"holderName\": \"海南公交集团\",  \"operateTime\": \"2019-05-09 11:45:21\",  \"bizAmount\": \"-700.00\",  \"rebate\": \"0.00\",  \"balance\": \"0.00\",  \"cardType\": \"公司账户\",  \"bizType\": \"注销\",  \"stationNo\": \"10-0001\",  \"stationName\": \"朗润集团\"}]"
+		  + "}";
 		  
 		  String url = "http://localhost:8080/gasStation/acceptStationData.do";
 		  String message = OkHttpClientUtil.httpPostJson(url,s);
